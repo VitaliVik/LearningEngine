a71ecc2937f4684288c23d77038f7c107ce22f99
Aleksei Garitski <a.harytski@andersenlab.com>
2020-02-05 17:58:14 +0300

    create unit for user case create theme

---
+++ LearningEngine.UnitTests/UseCase/CreateUserThemeFixture.cs
@@ -0,0 +1,76 @@
+﻿using LearningEngine.Domain.Command;
+using LearningEngine.Domain.Query;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LearningEngine.Domain.Enum;
+using MediatR;
+using LearningEngine.Domain.Interfaces;
+using System.Threading;
+using LearningEngine.Domain.DTO;
+using System.Linq;
+using Xunit;
+
+namespace LearningEngine.UnitTests.UseCase
+{
+    public class CreateUserThemeFixture
+    {
+        public CreateUserThemeFixture()
+        {
+            TestData = new CreateUserThemeTestData();
+            Mocks = new CreateUserThemeMocks(TestData);
+        }
+        public CreateUserThemeMocks Mocks { get; set; }
+        public CreateUserThemeTestData TestData { get; set; }
+
+        public class CreateUserThemeMocks
+        {
+            public CreateUserThemeMocks(CreateUserThemeTestData data)
+            {
+                MockMediator = new Mock<IMediator>();
+                MockUow = new Mock<ITransactionUnitOfWork>();
+                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => data.GetUserByNameQueryСorrect.UserName == q.UserName), CancellationToken.None))
+                    .ReturnsAsync(new UserDto { UserName = "rolit", Email = "rolit@mail.com" });
+                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => 
+                    data.CreateThemeCommandList.Any(dq => dq.ThemeName == c.ThemeName)
+                        ), CancellationToken.None))
+                    .ReturnsAsync(new Unit());
+                MockMediator.Setup(m => m.Send(It.Is<LinkUserToThemeCommand>(c => data.LinkCommandList.Any(dq =>
+                    c.ThemeName == dq.ThemeName &&
+                    c.UserName == dq.UserName &&
+                    (c.Access == dq.Access))), CancellationToken.None))
+                    .ReturnsAsync(new Unit());
+                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == null || q.UserName == ""), CancellationToken.None))
+                    .ReturnsAsync((UserDto)null);
+                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == null || c.ThemeName == ""), CancellationToken.None))
+                    .Throws(new Exception);
+                MockUow.Setup(m => m.CommitTransaction());
+                MockUow.Setup(m => m.StartTransaction());
+                MockUow.Setup(m => m.RollbackTransaction());
+            }
+            public Mock<IMediator> MockMediator { get; set; }
+            public Mock<ITransactionUnitOfWork> MockUow { get; set; }
+        }
+        public class CreateUserThemeTestData
+        {
+            public GetUserByNameQuery GetUserByNameQueryСorrect{ get; set; } = new GetUserByNameQuery("rolit");
+            public List<GetUserByNameQuery> GetUserByNameQueryIncorrect { get; set; } = new List<GetUserByNameQuery>
+            {
+                new GetUserByNameQuery(null),
+                new GetUserByNameQuery("")
+            };
+            public List<CreateThemeCommand> CreateThemeCommandList { get; set; } = new List<CreateThemeCommand>
+            {
+                new CreateThemeCommand("theme1", "description", true),
+                new CreateThemeCommand("theme2", null, true),
+                new CreateThemeCommand(null, null, true),
+            };
+            public List<LinkUserToThemeCommand> LinkCommandList { get; set; } = new List<LinkUserToThemeCommand>
+            {
+                new LinkUserToThemeCommand("rolit", "theme1", TypeAccess.Read | TypeAccess.Write),
+                new LinkUserToThemeCommand("rolit", "theme2", TypeAccess.Read | TypeAccess.Write),
+            };
+        }
+    }
+}
---
+++ LearningEngine.UnitTests/UseCase/CreateUserThemeUseCaseTest.cs
@@ -0,0 +1,49 @@
+﻿using LearningEngine.Application.UseCase.Command;
+using LearningEngine.Domain.Interfaces;
+using LearningEngine.Domain.Query;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LearningEngine.Domain.DTO;
+using LearningEngine.Domain.Command;
+using LearningEngine.Application.UseCase.Handler;
+using Xunit;
+
+namespace LearningEngine.UnitTests.UseCase
+{
+    public class CreateUserThemeUseCaseTest: IClassFixture<CreateUserThemeFixture>
+    {
+        CreateUserThemeFixture _fixture;
+        public CreateUserThemeUseCaseTest(CreateUserThemeFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task CreateUserThemeTest()
+        {
+        //    var command = new CreateUserThemeCommand("userName", "test", "test", true);
+        //    var handler = new CreateUserThemeHandler(_mockMediator.Object, _mockUow.Object);
+
+        //    var result = await handler.Handle(command, CancellationToken.None);
+
+        //    Assert.Equal(default, result);
+        //    _mockUow.Verify(m => m.StartTransaction(), Times.Once);
+        //    _mockMediator.Verify(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == "userName"), CancellationToken.None),
+        //    Times.Once);
+        //    _mockMediator.Verify(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == "test" &&
+        //        c.Description == "test" &&
+        //        c.IsPublic == true
+        //        ), CancellationToken.None), Times.Once);
+        //    _mockMediator.Verify(m => m.Send(It.Is<LinkUserToThemeCommand>(c => c.ThemeName == "test" &&
+        //        c.UserName == "userName" &&
+        //        (c.Access == (Domain.Enum.TypeAccess.Read | Domain.Enum.TypeAccess.Write)
+        //        )), CancellationToken.None), Times.Once);
+        //    _mockUow.Verify(m => m.CommitTransaction(), Times.Once);
+        }
+    }
+}
