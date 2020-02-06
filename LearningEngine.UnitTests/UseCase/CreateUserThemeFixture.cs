f480c229b07f63d9e7694b2f51505bedb729e7e5
Aleksei Garitski <a.harytski@andersenlab.com>
2020-02-06 12:24:31 +0300

    add unit test for createThemeUseCase

--- LearningEngine.UnitTests/UseCase/CreateUserThemeFixture.cs
+++ LearningEngine.UnitTests/UseCase/CreateUserThemeFixture.cs
@@ -1,76 +1,87 @@
-﻿using LearningEngine.Domain.Command;
-using LearningEngine.Domain.Query;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using LearningEngine.Domain.Enum;
-using MediatR;
-using LearningEngine.Domain.Interfaces;
-using System.Threading;
-using LearningEngine.Domain.DTO;
-using System.Linq;
-using Xunit;
+﻿//using LearningEngine.Domain.Command;
+//using LearningEngine.Domain.Query;
+//using Moq;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using LearningEngine.Domain.Enum;
+//using MediatR;
+//using LearningEngine.Domain.Interfaces;
+//using System.Threading;
+//using LearningEngine.Domain.DTO;
+//using System.Linq;
+//using Xunit;
+//using LearningEngine.Application.UseCase.Command;
+//using LearningEngine.Persistence.Models;
 
-namespace LearningEngine.UnitTests.UseCase
-{
-    public class CreateUserThemeFixture
-    {
-        public CreateUserThemeFixture()
-        {
-            TestData = new CreateUserThemeTestData();
-            Mocks = new CreateUserThemeMocks(TestData);
-        }
-        public CreateUserThemeMocks Mocks { get; set; }
-        public CreateUserThemeTestData TestData { get; set; }
+//namespace LearningEngine.UnitTests.UseCase
+//{
+//    public class CreateUserThemeFixture
+//    {
+//        public CreateUserThemeFixture()
+//        {
+//        }
 
-        public class CreateUserThemeMocks
-        {
-            public CreateUserThemeMocks(CreateUserThemeTestData data)
-            {
-                MockMediator = new Mock<IMediator>();
-                MockUow = new Mock<ITransactionUnitOfWork>();
-                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => data.GetUserByNameQueryСorrect.UserName == q.UserName), CancellationToken.None))
-                    .ReturnsAsync(new UserDto { UserName = "rolit", Email = "rolit@mail.com" });
-                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => 
-                    data.CreateThemeCommandList.Any(dq => dq.ThemeName == c.ThemeName)
-                        ), CancellationToken.None))
-                    .ReturnsAsync(new Unit());
-                MockMediator.Setup(m => m.Send(It.Is<LinkUserToThemeCommand>(c => data.LinkCommandList.Any(dq =>
-                    c.ThemeName == dq.ThemeName &&
-                    c.UserName == dq.UserName &&
-                    (c.Access == dq.Access))), CancellationToken.None))
-                    .ReturnsAsync(new Unit());
-                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == null || q.UserName == ""), CancellationToken.None))
-                    .ReturnsAsync((UserDto)null);
-                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == null || c.ThemeName == ""), CancellationToken.None))
-                    .Throws(new Exception);
-                MockUow.Setup(m => m.CommitTransaction());
-                MockUow.Setup(m => m.StartTransaction());
-                MockUow.Setup(m => m.RollbackTransaction());
-            }
-            public Mock<IMediator> MockMediator { get; set; }
-            public Mock<ITransactionUnitOfWork> MockUow { get; set; }
-        }
-        public class CreateUserThemeTestData
-        {
-            public GetUserByNameQuery GetUserByNameQueryСorrect{ get; set; } = new GetUserByNameQuery("rolit");
-            public List<GetUserByNameQuery> GetUserByNameQueryIncorrect { get; set; } = new List<GetUserByNameQuery>
-            {
-                new GetUserByNameQuery(null),
-                new GetUserByNameQuery("")
-            };
-            public List<CreateThemeCommand> CreateThemeCommandList { get; set; } = new List<CreateThemeCommand>
-            {
-                new CreateThemeCommand("theme1", "description", true),
-                new CreateThemeCommand("theme2", null, true),
-                new CreateThemeCommand(null, null, true),
-            };
-            public List<LinkUserToThemeCommand> LinkCommandList { get; set; } = new List<LinkUserToThemeCommand>
-            {
-                new LinkUserToThemeCommand("rolit", "theme1", TypeAccess.Read | TypeAccess.Write),
-                new LinkUserToThemeCommand("rolit", "theme2", TypeAccess.Read | TypeAccess.Write),
-            };
-        }
-    }
-}
+        
+
+//        public class Mocks
+//        {
+//            public Mocks(TestData td)
+//            {
+//                MockMediator = new Mock<IMediator>();
+//                MockUow = new Mock<ITransactionUnitOfWork>();
+//                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == "" || q.UserName == null), CancellationToken.None))
+//                    .Throws<Exception>();
+//                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => td.User.UserName == q.UserName), CancellationToken.None))
+//                    .ReturnsAsync(new UserDto { UserName = td.User.UserName});
+
+//                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c =>  
+//                td.CreateThemeCommand.ThemeName == c.ThemeName), CancellationToken.None))
+//                    .ReturnsAsync(new Unit());
+//                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == null || c.ThemeName == ""), CancellationToken.None))
+//                    .Throws<Exception>();
+
+//                MockMediator.Setup(m => m.Send(It.Is<LinkUserToThemeCommand>(
+//                    c => c.ThemeName == td.CreateThemeCommand.ThemeName && c.UserName == td.User.UserName), CancellationToken.None))
+//                    .ReturnsAsync(new Unit());
+
+//                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == null || q.UserName == ""), CancellationToken.None))
+//                    .ReturnsAsync((UserDto)null);
+//                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == null || c.ThemeName == ""), CancellationToken.None))
+//                    .Throws(new Exception);
+//                MockUow.Setup(m => m.CommitTransaction());
+//                MockUow.Setup(m => m.StartTransaction());
+//                MockUow.Setup(m => m.RollbackTransaction());
+//            }
+//            public Mock<IMediator> MockMediator { get; set; }
+//            public Mock<ITransactionUnitOfWork> MockUow { get; set; }
+//        }
+//        public class TestData
+//        {
+//            public TestData(string userName, string themeName)
+//            {
+//                User = new User { UserName = userName };
+//                CreateUserThemeCommand = new CreateUserThemeCommand(
+//                    User.UserName, 
+//                    themeName, 
+//                    "TestDescription", 
+//                    true);
+//                GetUserByNameQuery = new GetUserByNameQuery(CreateUserThemeCommand.UserName);
+//                CreateThemeCommand = new CreateThemeCommand(
+//                    CreateUserThemeCommand.ThemeName, 
+//                    CreateUserThemeCommand.Description, 
+//                    CreateUserThemeCommand.IsPublic);
+//                LinkUserToTheme = new LinkUserToThemeCommand(
+//                    User.UserName, 
+//                    CreateThemeCommand.ThemeName, 
+//                    TypeAccess.Read | TypeAccess.Write);
+//            }
+//            public User User { get; set; } 
+//            public CreateUserThemeCommand CreateUserThemeCommand { get; set; } 
+//            public GetUserByNameQuery GetUserByNameQuery { get; set; }
+//            public CreateThemeCommand CreateThemeCommand { get; set; }
+//            public LinkUserToThemeCommand LinkUserToTheme { get; set; }
+
+//        }
+//    }
+//}
--- LearningEngine.UnitTests/UseCase/CreateUserThemeUseCaseTest.cs
+++ LearningEngine.UnitTests/UseCase/CreateUserThemeUseCaseTest.cs
@@ -12,38 +12,130 @@ using LearningEngine.Domain.DTO;
 using LearningEngine.Domain.Command;
 using LearningEngine.Application.UseCase.Handler;
 using Xunit;
+using LearningEngine.Persistence.Models;
+using LearningEngine.Domain.Enum;
 
 namespace LearningEngine.UnitTests.UseCase
 {
-    public class CreateUserThemeUseCaseTest: IClassFixture<CreateUserThemeFixture>
+    public class CreateUserThemeUseCaseTest
     {
-        CreateUserThemeFixture _fixture;
-        public CreateUserThemeUseCaseTest(CreateUserThemeFixture fixture)
-        {
-            _fixture = fixture;
-        }
 
         [Fact]
-        public async Task CreateUserThemeTest()
+        public async Task Success()
         {
-        //    var command = new CreateUserThemeCommand("userName", "test", "test", true);
-        //    var handler = new CreateUserThemeHandler(_mockMediator.Object, _mockUow.Object);
+            //arange
+            var td = new TestData("rolit", "test");
+            var mocks = new Mocks(td);
 
-        //    var result = await handler.Handle(command, CancellationToken.None);
+            //act
+            var result = await mocks.TestingHandler.Handle(td.CreateUserThemeCommand, CancellationToken.None);
 
-        //    Assert.Equal(default, result);
-        //    _mockUow.Verify(m => m.StartTransaction(), Times.Once);
-        //    _mockMediator.Verify(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == "userName"), CancellationToken.None),
-        //    Times.Once);
-        //    _mockMediator.Verify(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == "test" &&
-        //        c.Description == "test" &&
-        //        c.IsPublic == true
-        //        ), CancellationToken.None), Times.Once);
-        //    _mockMediator.Verify(m => m.Send(It.Is<LinkUserToThemeCommand>(c => c.ThemeName == "test" &&
-        //        c.UserName == "userName" &&
-        //        (c.Access == (Domain.Enum.TypeAccess.Read | Domain.Enum.TypeAccess.Write)
-        //        )), CancellationToken.None), Times.Once);
-        //    _mockUow.Verify(m => m.CommitTransaction(), Times.Once);
+            //assert
+            mocks.MockUow.Verify(_ => _.StartTransaction(), Times.Once);
+            mocks.MockMediator.Verify(_ => _.Send(It.Is<GetUserByNameQuery>(
+                c => c.UserName == td.GetUserByNameQuery.UserName), CancellationToken.None), Times.Once);
+            mocks.MockMediator.Verify(_ => _.Send(It.Is<CreateThemeCommand>(
+                c => c.ThemeName == td.CreateThemeCommand.ThemeName), CancellationToken.None), Times.Once);
+            mocks.MockMediator.Verify(_ => _.Send(It.IsAny<LinkUserToThemeCommand>(), CancellationToken.None), Times.Once);
+            mocks.MockUow.Verify(_ => _.CommitTransaction(), Times.Once);
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task ThrowExceptionUserNotFound(string userName)
+        {
+            //arange
+            var td = new TestData(userName, "test");
+            var mocks = new Mocks(td);
+
+            //act
+            Func<Task<Unit>> act =  () => mocks.TestingHandler.Handle(td.CreateUserThemeCommand, CancellationToken.None);
+
+            //assert
+            await Assert.ThrowsAsync<Exception>(act);
+            mocks.MockUow.Verify(_ => _.StartTransaction(), Times.Once);
+            mocks.MockUow.Verify(_ => _.RollbackTransaction(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+
+        public async Task ThrowExceptionIncorrectTheme(string themeName)
+        {
+            //arange
+            var td = new TestData("user", themeName);
+            var mocks = new Mocks(td);
+
+            //act
+            Func<Task<Unit>> act = () => mocks.TestingHandler.Handle(td.CreateUserThemeCommand, CancellationToken.None);
+
+            //assert
+            await Assert.ThrowsAsync<Exception>(act);
+            mocks.MockUow.Verify(_ => _.StartTransaction(), Times.Once);
+            mocks.MockUow.Verify(_ => _.RollbackTransaction(), Times.Once);
+        }
+
+        
+
+        public class Mocks
+        {
+            public Mocks(TestData td)
+            {
+                MockMediator = new Mock<IMediator>();
+                MockUow = new Mock<ITransactionUnitOfWork>();
+                MockMediator.Setup(m => m.Send(It.Is<GetUserByNameQuery>(q => q.UserName == "" || q.UserName == null), CancellationToken.None))
+                    .ReturnsAsync((UserDto)null);
+                MockMediator.Setup(m => m.Send(
+                    It.Is<GetUserByNameQuery>(q => td.User.UserName == q.UserName && (td.User.UserName != null && td.User.UserName != "")), CancellationToken.None))
+                    .ReturnsAsync(new UserDto { UserName = td.User.UserName });
+
+                MockMediator.Setup(m => m.Send(It.IsAny<CreateThemeCommand>(), CancellationToken.None))
+                    .ReturnsAsync(new Unit());
+                MockMediator.Setup(m => m.Send(It.Is<CreateThemeCommand>(c => c.ThemeName == null || c.ThemeName == ""), CancellationToken.None))
+                    .Throws<Exception>();
+
+                MockMediator.Setup(m => m.Send(It.IsAny<LinkUserToThemeCommand>(), CancellationToken.None))
+                    .ReturnsAsync(new Unit());
+
+                MockUow.Setup(m => m.CommitTransaction());
+                MockUow.Setup(m => m.StartTransaction());
+                MockUow.Setup(m => m.RollbackTransaction());
+                TestingHandler = new CreateUserThemeHandler(MockMediator.Object, MockUow.Object);
+            }
+            public Mock<IMediator> MockMediator { get; set; }
+            public Mock<ITransactionUnitOfWork> MockUow { get; set; }
+            public CreateUserThemeHandler TestingHandler { get; set; }
+
+        }
+        public class TestData
+        {
+            public TestData(string userName, string themeName)
+            {
+                User = new User { UserName = userName };
+                CreateUserThemeCommand = new CreateUserThemeCommand(
+                    User.UserName,
+                    themeName,
+                    "TestDescription",
+                    true);
+                GetUserByNameQuery = new GetUserByNameQuery(CreateUserThemeCommand.UserName);
+                CreateThemeCommand = new CreateThemeCommand(
+                    CreateUserThemeCommand.ThemeName,
+                    CreateUserThemeCommand.Description,
+                    CreateUserThemeCommand.IsPublic);
+                LinkUserToTheme = new LinkUserToThemeCommand(
+                    User.UserName,
+                    CreateThemeCommand.ThemeName,
+                    TypeAccess.Read | TypeAccess.Write);
+            }
+            public User User { get; set; }
+            public CreateUserThemeCommand CreateUserThemeCommand { get; set; }
+            public GetUserByNameQuery GetUserByNameQuery { get; set; }
+            public CreateThemeCommand CreateThemeCommand { get; set; }
+            public LinkUserToThemeCommand LinkUserToTheme { get; set; }
+
         }
     }
 }
